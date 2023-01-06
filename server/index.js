ebcdd6be432b89eb0fcc754fbc2411946091fbb6
Muhammed ali Erdel <nagonad@gmail.com>
2023-01-06 12:01:48 +0200

    express static

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../build")));
 
 //get all menu
 app.get("/menu", async (req, res) => {
