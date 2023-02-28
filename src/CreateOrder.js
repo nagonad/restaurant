08ef92fc682a6a020744869ce586693f5246fb38
Muhammed ali Erdel <nagonad@gmail.com>
2023-02-28 23:42:53 +0100

    createorder component updated

--- src/CreateOrder.js
+++ src/CreateOrder.js
@@ -1,120 +1,283 @@
-import React, { Component } from "react";
-import { Table, Input, Button } from "reactstrap";
-import Modal from "react-modal";
-import CheckoutModal from "./CheckoutModal";
+import * as React from "react";
+import { experimentalStyled as styled } from "@mui/material/styles";
 
-Modal.setAppElement(document.getElementById("root"));
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Slide from "@mui/material/Slide";
+import CloseIcon from "@mui/icons-material/Close";
+import Box from "@mui/material/Box";
 
-export default class CreateOrder extends Component {
-  state = {
-    showModal: false,
-    currentOrderItem: {},
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function CreateOrder(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const [openSecond, setOpenSecond] = React.useState(false);
+
+  const [selectedSizeForCart, setSelectedSizeForCart] = React.useState();
+
+  const [selectedProductForCart, setSelectedProductForCart] = React.useState();
+
+  const [selectedProduct, setSelectedProduct] = React.useState();
+
+  const [variants, setVariants] = React.useState();
+
+  const [cart, setCart] = React.useState([]);
+
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  toggleModal = () => {
-    this.setState({ showModal: !this.state.showModal });
+  const handleClose = () => {
+    setOpen(false);
   };
 
-  handleCurrentOrderItem = (product, size) => {
-    let currentOrderItem = this.state.currentOrderItem;
-    currentOrderItem.product = product;
-    currentOrderItem.size = size;
+  const getSP = (product) => {
+    props
+      .getSelectedProduct(product)
+      .then((response) => response.json())
+      .then((data) => {
+        let selected = [];
 
-    this.setState({ currentOrderItem: currentOrderItem });
+        data.forEach((datum) => {
+          if (datum.selected) {
+            selected.push(datum);
+          }
+        });
+
+        setSelectedProduct(selected);
+      });
   };
 
-  render() {
-    return (
+  const getSV = (size) => {
+    props
+      .getSizeVariantById(size.id)
+      .then((resp) => resp.json())
+      .then((data) => {
+        let newData = [];
+        let obj = {};
+        data.forEach((datum) => {
+          obj = { ...datum, orderSelected: false };
+          newData.push(obj);
+        });
+
+        setVariants(newData);
+      });
+  };
+
+  const setOrderSelect = (variant) => {
+    let vary = variants.map((a) => {
+      return { ...a };
+    });
+
+    let varElement = vary.find((c) => c.id === variant.id);
+
+    varElement.orderSelected = !varElement.orderSelected;
+    setVariants(vary);
+  };
+
+  const saveItemToTheCart = () => {
+    handleClose();
+    setOpenSecond(false);
+
+    let cartObj = {};
+
+    cartObj.size = selectedSizeForCart;
+
+    cartObj.product = selectedProductForCart;
+
+    let cartObjVariants = [];
+
+    variants.forEach((variant) => {
+      if (variant.orderSelected) {
+        cartObjVariants.push(variant);
+      }
+    });
+
+    cartObj.variants = cartObjVariants;
+
+    let newCart = cart;
+
+    newCart.push(cartObj);
+
+    console.log(newCart);
+
+    setCart(newCart);
+  };
+
+  return (
+    <Grid>
       <div>
-        <Table striped size="sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Product Name</th>
-
-              <th>Klein</th>
-              <th>Mittel</th>
-              <th>Groß</th>
-              <th>Grand</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.catProducts.map((product) => (
-              <tr key={product.productId}>
-                <th scope="row">{product.productId}</th>
-                <td>{product.productName}</td>
-
-                <td>
-                  {product.unitPrice.Klein ? (
-                    <Button
+        <Dialog
+          fullScreen
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Transition}
+        >
+          <AppBar sx={{ position: "relative" }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={handleClose}
+                aria-label="close"
+              >
+                <CloseIcon />
+              </IconButton>
+              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                Sound
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <List>
+            {selectedProduct
+              ? selectedProduct.map((size) => (
+                  <div key={size.id}>
+                    <ListItem
                       onClick={() => {
-                        this.handleCurrentOrderItem(product, "Klein");
-                        this.props.setOrderNumber();
-                        this.toggleModal();
+                        setSelectedSizeForCart(size);
+                        getSV(size);
+                        setOpenSecond(true);
                       }}
+                      button
                     >
-                      {product.unitPrice.Klein}
-                    </Button>
-                  ) : (
-                    <></>
-                  )}
-                </td>
-                <td>
-                  {product.unitPrice.Mittel ? (
-                    <Button
-                      onClick={() => {
-                        this.handleCurrentOrderItem(product, "Mittel");
-                        this.props.setOrderNumber();
-                        this.toggleModal();
-                      }}
-                    >
-                      {product.unitPrice.Mittel}
-                    </Button>
-                  ) : (
-                    <></>
-                  )}
-                </td>
-                <td>
-                  {product.unitPrice.Groß ? (
-                    <Button
-                      onClick={() => {
-                        this.handleCurrentOrderItem(product, "Groß");
-                        this.props.setOrderNumber();
-                        this.toggleModal();
-                      }}
-                    >
-                      {product.unitPrice.Groß}
-                    </Button>
-                  ) : (
-                    <></>
-                  )}
-                </td>
-                <td>
-                  {product.unitPrice.Grand ? (
-                    <Button
-                      onClick={() => {
-                        this.handleCurrentOrderItem(product, "Grand");
-                        this.props.setOrderNumber();
-                        this.toggleModal();
-                      }}
-                    >
-                      {product.unitPrice.Grand}
-                    </Button>
-                  ) : (
-                    <></>
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-        <CheckoutModal
-          isOpen={this.state.showModal}
-          toggle={this.toggleModal}
-          addToCart={this.props.addToCart}
-          currentOrderItem={this.state.currentOrderItem}
-        ></CheckoutModal>
+                      <ListItemText
+                        primary={size.sizename}
+                        secondary={size.unitprice}
+                      />
+                    </ListItem>
+                    <Divider />
+                  </div>
+                ))
+              : null}
+          </List>
+          <Dialog
+            fullScreen
+            open={openSecond}
+            onClose={handleClose}
+            TransitionComponent={Transition}
+          >
+            <AppBar sx={{ position: "relative" }}>
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  onClick={() => setOpenSecond(false)}
+                  aria-label="close"
+                >
+                  <CloseIcon />
+                </IconButton>
+                <Typography
+                  sx={{ ml: 2, flex: 1 }}
+                  variant="h6"
+                  component="div"
+                >
+                  Ses
+                </Typography>
+                <Button
+                  autoFocus
+                  color="inherit"
+                  onClick={() => {
+                    saveItemToTheCart();
+                  }}
+                >
+                  save
+                </Button>
+              </Toolbar>
+            </AppBar>
+            <Grid container spacing={{ xs: 2, md: 3 }} padding={2}>
+              {variants
+                ? variants.map((variant) => (
+                    <Grid item xs={6} sm={4} md={3} key={variant.sizevariantid}>
+                      <Item
+                        sx={{
+                          boxShadow: variant.orderSelected
+                            ? "inset 0 0 10px rgba(0, 0, 0, 0.5)"
+                            : "",
+                        }}
+                        onClick={() => {
+                          setOrderSelect(variant);
+                        }}
+                      >
+                        <div>{variant.variantname}</div>
+                      </Item>
+                    </Grid>
+                  ))
+                : null}
+            </Grid>
+          </Dialog>
+        </Dialog>
       </div>
-    );
-  }
+      <Box sx={{ display: "flex" }}>
+        <Box sx={{ width: "75%" }}>
+          <Grid
+            container
+            rowSpacing={2}
+            paddingX={2}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {props.products.map((product) => (
+              <React.Fragment key={product.id}>
+                <Grid
+                  onClick={() => {
+                    setSelectedProductForCart(product);
+                    getSP(product);
+                    handleClickOpen(true);
+                  }}
+                  item
+                  xs={6}
+                  md={4}
+                >
+                  <Item>
+                    {product.productid} - {product.productname}
+                  </Item>
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Box>
+
+        <Paper
+          sx={{
+            width: "25%",
+            height: "100vh",
+            position: "fixed",
+            right: "0",
+            top: "0",
+          }}
+        >
+          <Typography variant="h6" align="center" marginY={2}>
+            Dein Bestellung
+          </Typography>
+          <Divider></Divider>
+          {cart.map((cartItem) => (
+            <Box padding={1}>
+              {cartItem.product.productid} - {cartItem.product.productname} - (
+              {cartItem.size.sizename})
+            </Box>
+          ))}
+        </Paper>
+      </Box>
+    </Grid>
+  );
 }
