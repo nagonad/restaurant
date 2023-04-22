5134f0701ada895e6a96f734a70217d9f9cb34fd
Muhammed ali Erdel <nagonad@gmail.com>
2023-04-23 00:03:43 +0200

    ddddd

--- server/db.js
+++ server/db.js
@@ -9,27 +9,27 @@ const fs = require("fs");
 //   database: "postgres",
 // });
 
-const pool = new Pool({
-  user: "avnadmin",
-  password: "AVNS_GOnsdnzA0FusC-UiNHh",
-  host: "pg-restaurant-nagonad.aivencloud.com",
-  port: 13043,
-  database: "restaurant",
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync(__dirname + "/ca.pem").toString(),
-  },
-});
-
 // const pool = new Pool({
-//   user: "reader",
-//   password: "NWDMCE5xdipIjRrp",
-//   host: "hh-pgsql-public.ebi.ac.uk",
-//   port: 5432,
-//   database: "pfmegrnargs",
+//   user: "avnadmin",
+//   password: "AVNS_GOnsdnzA0FusC-UiNHh",
+//   host: "pg-restaurant-nagonad.aivencloud.com",
+//   port: 13043,
+//   database: "restaurant",
+//   ssl: {
+//     rejectUnauthorized: true,
+//     ca: fs.readFileSync(__dirname + "/ca.pem").toString(),
+//   },
 // });
 
-// pool.query("select now()").then((data) => console.log(data));
+const pool = new Pool({
+  user: "reader",
+  password: "NWDMCE5xdipIjRrp",
+  host: "hh-pgsql-public.ebi.ac.uk",
+  port: 5432,
+  database: "pfmegrnargs",
+});
+
+pool.query("select now()").then((data) => console.log(data));
 
 // pool.connect();
 
